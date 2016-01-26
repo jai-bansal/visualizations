7accdd30d38e64ffed60293f1fc0c00d6a560293
jai-bridg <jai@bridg.com>
2016-01-26 12:11:41 -0800

    finished data scraping file
    finished data scraping file ('census_data_scrape_and_process.py').
    resulting csv is 'census_data.csv'

--- census_data_scrape_and_process.py
+++ census_data_scrape_and_process.py
@@ -5,10 +5,10 @@
 # This data comes from the United States Census and can be viewed at the url below.
 # https://en.wikipedia.org/wiki/List_of_U.S._states_by_historical_population
 
-# Import modules.
+# IMPORT MODULES:
 import pandas as pd
 
-# Scrape data.
+# SCRAPE DATA:
 # 'pandas read_html' will search for all tables on the provided url as long as they match the 'match' parameter.
 # I've provided a 'match' parameter of '2010' as the table I want turns out to be the only table with this value.
 # I personally think this is a silly way of scraping and that I should be able to pass in the xpath. Anyway, this works.
@@ -19,3 +19,15 @@ census_data = pd.read_html('https://en.wikipedia.org/wiki/List_of_U.S._states_by
 # Convert census_data to 'DataFrame'.
 # 'pandas read_html' returns a list, so I extract the first element.
 census_data = census_data[0]
+
+# Rename columns (header row was not recognized):
+census_data.columns = ['Name', '1960', '1970', '1980', '1990', '2000', '2010']
+
+# Drop duplicate header row:
+census_data = census_data[census_data['Name'] != 'Name']
+
+# Sort 'census_data' by 'Name':
+census_data.sort_values( by = 'Name', inplace = True)
+
+# CREATE CSV FILE:
+census_data.to_csv('census_data.csv')

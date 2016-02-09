a2c1960265ea995480ace3911883e49ac7beb5e7
jai-bridg <jai@bridg.com>
2016-02-09 10:41:58 -0800

    added forgotten package import
    forgot to import 'ggthemes'

--- line_graph.R
+++ line_graph.R
@@ -8,6 +8,7 @@
 library(data.table)
 library(reshape2)
 library(ggplot2)
+library(ggthemes)
 
 # Load data.
 census_data = data.table(read.csv('census_data.csv', header = T, stringsAsFactors = F, check.names = F))

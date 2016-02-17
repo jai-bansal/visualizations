e93461ca284e9e92507a7e5207da9a37b6c4567d
jai-bridg <jai@bridg.com>
2016-02-17 09:34:38 -0800

    modify scatter plot
    increase size of all plot text. change population scale from thousands
    to millions. add new image.

--- scatter_plot.R
+++ scatter_plot.R
@@ -29,13 +29,13 @@ census_data = data.table(read.csv('census_data.csv',
   # Create data subset (specified above).
   census_subset = census_data[Name %in% states]
   
-  # Divided population values by 1000 for easier graph viewing.
-  census_subset$`1960` = census_subset$`1960` / 1000
-  census_subset$`1970` = census_subset$`1970` / 1000
-  census_subset$`1980` = census_subset$`1980` / 1000
-  census_subset$`1990` = census_subset$`1990` / 1000
-  census_subset$`2000` = census_subset$`2000` / 1000
-  census_subset$`2010` = census_subset$`2010` / 1000
+  # Divided population values by 1000000 for easier graph viewing.
+  census_subset$`1960` = census_subset$`1960` / 1000000
+  census_subset$`1970` = census_subset$`1970` / 1000000
+  census_subset$`1980` = census_subset$`1980` / 1000000
+  census_subset$`1990` = census_subset$`1990` / 1000000
+  census_subset$`2000` = census_subset$`2000` / 1000000
+  census_subset$`2010` = census_subset$`2010` / 1000000
   
   # Melt 'census_subset' to allow creation of scatter plot.
   census_subset = melt(census_subset, 
@@ -54,22 +54,22 @@ ggplot(data = census_subset,
            color = State, 
            shape = State)) +
   geom_point(size = 4) +
-  scale_y_continuous(breaks = seq(2500, 20000, by = 2500)) +
+  scale_y_continuous(breaks = seq(2.5, 20, by = 2.5)) +
   theme(axis.text.x = element_text(color = 'black', 
-                                   size = 16), 
+                                   size = 17), 
         axis.text.y = element_text(color = 'black', 
-                                   size = 16), 
+                                   size = 17), 
         axis.title.x = element_text(face = 'bold', 
-                                    size = 16, 
+                                    size = 17, 
                                     vjust = -0.25), 
         axis.title.y = element_text(face = 'bold', 
-                                    size = 16, 
+                                    size = 17, 
                                     vjust = 1), 
         plot.title = element_text(face = 'bold',
-                                  size = 18, 
+                                  size = 19, 
                                   vjust = 1.5), 
-        legend.title = element_text(size = 18), 
-        legend.text = element_text(size = 17)) +
-  ggtitle('Selected State Populations (in thousands) Over Time') +
+        legend.title = element_text(size = 19), 
+        legend.text = element_text(size = 18)) +
+  ggtitle('Selected State Populations (in millions) Over Time') +
   xlab('Year') +
-  ylab('Population (in thousands)')
+  ylab('Population (in millions)')

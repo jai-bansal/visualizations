282a1267846c4ac6351fbc0965c9dc844e1d3cb0
jai-bridg <jai@bridg.com>
2017-04-16 18:46:16 -0700

    add interactive plots
    - make multi plot grouped bar graph interactive
    - add interactive 3D scatter script and plot
    ('ggplot_and_plotly\3d_scatter_plot_interactive.R' and
    'ggplot_and_plotly\3d_scatter_plot_interactive.png' respectively)

---
+++ ggplot_and_plotly/3d_scatter_plot_interactive.R
@@ -0,0 +1,100 @@
+# This script creates a 3 dimensional scatter plot using (a subset of) 'census_data.csv'.
+
+# A description of 'census_data.csv' can be found in 'census_data_scrape_and_process.R'.
+# To avoid an overwhelming plot, I will be using a subset of 'census_data.csv' consisting of
+# observations for 4 states (Maryland, Pennsylvania, Virginia, and West Virginia) in 2010.
+
+# Load packages.
+library(data.table)
+library(plotly)
+
+# Load data.
+# Note that the data is located in the 'visualizations' folder, NOT the 'ggplot' folder.
+census_data = data.table(read.csv('census_data.csv', 
+                                  header = T, 
+                                  stringsAsFactors = F, 
+                                  check.names = F))
+
+#####
+# SUBSET/PROCESS DATA.
+
+  # Specify states of interest.
+  states = c('Maryland', 'Pennsylvania', 'Virginia', 'West Virginia')
+
+  # Subset data.
+  census_subset = census_data[Name %in% states]
+
+  # Divide population values by 1000000 for easier graph viewing.
+  census_subset$`1960` = census_subset$`1960` / 1000000
+  census_subset$`1970` = census_subset$`1970` / 1000000
+  census_subset$`1980` = census_subset$`1980` / 1000000
+  census_subset$`1990` = census_subset$`1990` / 1000000
+  census_subset$`2000` = census_subset$`2000` / 1000000
+  census_subset$`2010` = census_subset$`2010` / 1000000
+  
+  # Divide area values by 1000 for easier graph viewing.
+  census_subset$`Area (sq. miles)` = census_subset$`Area (sq. miles)` / 1000
+  
+  # Keep state areas in separate data frame and rename columns.
+  areas = census_subset[, .(Name, `Area (sq. miles)`)]
+  setnames(areas, 
+           names(areas), 
+           c('State', 'Area (sq. miles)'))
+  
+  # Remove 'Area (sq. miles) from 'census_subset' to allow melt.
+  census_subset$`Area (sq. miles)` = NULL
+    
+  # Melt 'census_subset' to allow 3 dimensional plot and rename columns.
+  census_subset = melt(census_subset, 
+                       id = 'Name')
+  setnames(census_subset, 
+           names(census_subset), 
+           c('State', 'Year', 'Population'))
+  
+  # Add 'areas' to 'census_subset'.
+  census_subset = merge(census_subset, 
+                        areas, 
+                        by = 'State')
+  
+  # Change 'census_subset$Year' to character vector.
+  census_subset$Year = as.character(census_subset$Year)
+  
+  # Add 'colors' column.
+  # Each state gets a color which will be that state's color in the plot.
+  census_subset$colors = ifelse(census_subset$State == 'Maryland', 'blue', 
+                                ifelse(census_subset$State == 'Pennsylvania', 'red', 
+                                       ifelse(census_subset$State == 'Virginia', 'green', 'orange')))
+  
+  # Add a 'shape_number' column.
+  # Each state gets a number which will correspond to that state's shape in the plot.
+  census_subset$shape_number = ifelse(census_subset$State == 'Maryland', 15, 
+                                      ifelse(census_subset$State == 'Pennsylvania', 16, 
+                                             ifelse(census_subset$State == 'Virginia', 17, 18)))
+
+#####
+# PLOT DATA.
+
+  # Create plot.
+  plot_ly(data = census_subset, 
+          x = ~Population, 
+          y = ~Year, 
+          z = ~`Area (sq. miles)`, 
+          color = ~State, 
+          colors = c('#ff0000', '#0066ff', '#339933', '#ff9900'), 
+          type = 'scatter3d', 
+          mode = 'lines+markers', 
+          hoverinfo = 'text', 
+          text = ~paste0('Population: ', 
+                         round(census_subset$Population, 1), 
+                         'M', 
+                         '<br>', 
+                         'Year: ', 
+                         census_subset$Year, 
+                         '<br>', 
+                         'Area: ',
+                         round(census_subset$`Area (sq. miles)`, 1), 
+                         ' thousand sq. miles')) %>% 
+      layout(title = 'Selected State Populations and Areas Over Time', 
+             scene = list(xaxis = list(title = 'Pop. (millions)'), 
+                          yaxis = list(title = 'Year'), 
+                          zaxis = list(title = 'Area (1000s of sq. miles)')))
---
+++ ggplot_and_plotly/multi_plot_grouped_bar_graph_interactive.R
@@ -0,0 +1,132 @@
+# This script creates a figure consisting of two subplots using (a subset of) 'census_data.csv' using the 'ggplot2' library.
+
+# A description of 'census_data.csv' can be found in 'census_data_scrape_and_process.R'.
+# To avoid an overwhelming plot, I will be using a subset of 'census_data.csv'.
+
+# The first subplot will be a grouped bar graph that uses all observations for 3 states: Vermont, Delaware, and Ohio
+# It will show the percentage growth between decades for each state.
+# The second subplot will be a bar graph that uses the aggregate United States observations
+# and shows the percentage growth between decades for the entire United States.
+
+# Population percentage growth in a decade 't' will be computed as:
+# 100 * (population(t + 1) - population(t)) / population(t)
+
+# For example, the population growth between 1960 and 1970 is:
+# 100 * (population(1970) - population(1960)) / population(1960)
+
+# Load packages.
+library(data.table)
+library(ggplot2)
+library(plotly)
+
+# Load data.
+# Note that the data is located in the 'visualizations' folder, NOT the 'ggplot' folder.
+census_data = data.table(read.csv('census_data.csv', 
+                                  header = T, 
+                                  stringsAsFactors = F, 
+                                  check.names = F))
+
+#####
+# SUBSET/PROCESS DATA.
+
+  # Specify rows of interest (states specified above and aggregate 'United States').
+  interest = c('Vermont', 'Delaware', 'Ohio', 'United States')
+
+  # Subset data.
+  census_subset = census_data[Name %in% interest]
+  
+  # Compute percentage growth during 1960s decade (between 1960 and 1970):
+  census_subset$`1960s_growth` = 100 * (census_subset$`1970` - census_subset$`1960`) / census_subset$`1960`
+  
+  # Compute percentage growth during 1970s decade (between 1970 and 1980):
+  census_subset$`1970s_growth` = 100 * (census_subset$`1980` - census_subset$`1970`) / census_subset$`1970`
+
+  # Compute percentage growth during 1980s decade (between 1980 and 1990):
+  census_subset$`1980s_growth` = 100 * (census_subset$`1990` - census_subset$`1980`) / census_subset$`1980`
+
+  # Compute percentage growth during 1990s decade (between 1990 and 2000):
+  census_subset$`1990s_growth` = 100 * (census_subset$`2000` - census_subset$`1990`) / census_subset$`1990`
+
+  # Compute percentage growth during 2000s decade (between 2000 and 2010):
+  census_subset$`2000s_growth` = 100 * (census_subset$`2010` - census_subset$`2000`) / census_subset$`2000`
+  
+  # Remove unneeded columns (all actual population measurements):
+  census_subset = census_subset[, .(Name, `1960s_growth`, `1970s_growth`, `1980s_growth`, `1990s_growth`, `2000s_growth`)]
+  
+  # Melt data to allow plotting.
+  census_subset = melt(census_subset, 
+                       id = 'Name')
+
+#####
+# PLOT DATA.
+  
+  # Create state data plot.
+  plot_1 = ggplotly(ggplot(data = census_subset[Name != 'United States'], 
+                  aes(x = variable, 
+                      y = value)) +
+            geom_bar(aes(fill = Name), 
+                     position = 'dodge', 
+                     stat = 'identity', 
+                     width = 0.9) +
+            scale_y_continuous(breaks = seq(0, 25, by = 5), 
+                               limits = c(0, 25)) +
+            theme(axis.text.x =  element_text(color = 'black', 
+                                      size = 14), 
+                  axis.text.y = element_text(color = 'black', 
+                                     size = 14), 
+                  axis.title.x = element_text(face = 'bold', 
+                                    size = 14, 
+                                    vjust = -0.5), 
+                  axis.title.y = element_text(face = 'bold', 
+                                    size = 14, 
+                                    vjust = 1), 
+                  plot.title = element_text(face = 'bold',
+                                  size = 16, 
+                                  vjust = 2), 
+                  legend.title = element_text(size = 16), 
+                  legend.text = element_text(size = 16), 
+                  legend.title.align = 0.5, 
+                  legend.text.align = 0) +
+                  guides(fill = guide_legend(title = '')) +
+                  scale_x_discrete(labels = c('1960s', '1970s', '1980s', '1990s', '2000s')) +
+            xlab('') +
+            ylab('% Growth'))
+
+  # Create aggregate United States plot.
+  plot_2 = ggplotly(ggplot(data = census_subset[Name == 'United States'], 
+                  aes(x = variable, 
+                      y = value)) +
+            geom_bar(aes(fill = Name), 
+                     position = 'dodge', 
+                     stat = 'identity',  
+                     width = 0.3, 
+                     color = 'black') +
+            scale_y_continuous(breaks = seq(0, 25, by = 5), 
+                               limits = c(0, 25)) +
+            scale_fill_manual(values = 'orange') +
+            theme(axis.text.x =  element_text(color = 'black', 
+                                      size = 14), 
+                  axis.text.y = element_text(color = 'black', 
+                                     size = 14), 
+                  axis.title.x = element_text(face = 'bold', 
+                                    size = 14, 
+                                    vjust = -0.5), 
+                  axis.title.y = element_text(face = 'bold', 
+                                    size = 14, 
+                                    vjust = 1), 
+                  plot.title = element_text(face = 'bold',
+                                  size = 16, 
+                                  vjust = 2), 
+                  legend.title = element_text(size = 16), 
+                  legend.text = element_text(size = 16), 
+                  legend.title.align = 0.5,
+                  legend.text.align = 0) +
+                  scale_x_discrete(labels = c('1960s', '1970s', '1980s', '1990s', '2000s')) +
+                  guides(fill = guide_legend(title = '')) +
+            ggtitle('% Growth by Decade for Selected States and US') +
+            xlab('Decade') +
+            ylab('% Growth'))
+  
+  # Plot both subplots.
+  subplot(plot_1, plot_2, 
+          nrows = 2, shareX = T, shareY = T)

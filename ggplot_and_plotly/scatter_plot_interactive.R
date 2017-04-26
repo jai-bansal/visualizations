057fc9cbf52bf13e2ab14f6d14b537480e19bfe9
jai-bansal <jai@bridg.com>
2017-04-25 17:45:43 -0700

    remove incorrect comment

--- ggplot_and_plotly/scatter_plot_interactive.R
+++ ggplot_and_plotly/scatter_plot_interactive.R
@@ -1,9 +1,6 @@
 # This script creates a scatter plot using (a subset of) 'census_data.csv' using the 'ggplot2' and 'plotly' libraries.
 
 # A description of 'census_data.csv' can be found in 'census_data_scrape_and_process.R'.
-# To avoid an overwhelming plot, I will be using a subset of 'census_data.csv' consisting of
-# all observations for 6 states (North Carolina, Maryland, New Jersey, Pennsylvania, Virginia, 
-# and New York).
 
 # Load packages.
 library(data.table)

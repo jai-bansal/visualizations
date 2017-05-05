9b3152cd42174e67349fb3ac5de465e11c2e9945
jai-bansal <jbans79@gmail.com>
2017-05-05 16:50:08 -0700

    progress on bar graph

--- plotly/bar_graph_interactive.py
+++ plotly/bar_graph_interactive.py
@@ -7,15 +7,19 @@
 ################
 
 # Import modules.
+import os
 import pandas as pd
 import numpy as np
-import plotly.plotly as py
-import plotly.graph_objs as go
+import plotly
+import plotly.graph_objs as pgo
 
 #############
 # IMPORT DATA
 #############
-# Note that the data is located in the 'visualizations' folder.
+
+# Set working directory.
+# This obviously needs to be changed depending on the computer being used.
+os.chdir('D:\\Users\\JBansal\\Documents\\GitHub\\visualizations')
 
 # Load data.
 census_data = pd.read_csv('census_data.csv',
@@ -25,4 +29,34 @@ census_data = pd.read_csv('census_data.csv',
 # TRANSFORM DATA
 ################
 
+# Specify states of interest.
+states = ['Delaware', 'Maryland', 'New Jersey', 'Pennsylvania', 'Virginia', 'West Virginia']
 
+# Create data subset.
+census_subset = census_data[census_data['Name'].isin(states)]
+census_subset = census_subset[['Name', '2010']]
+
+# Reset 'census_subset' index.
+census_subset.reset_index(drop = True,
+                          inplace = True)
+
+# Divide population values by 1000000 for easier graph viewing.
+census_subset['2010'] = census_subset['2010'] / 1000000
+
+###########
+# PLOT DATA
+###########
+
+plotly.offline.plot({
+    "data": [
+        pgo.Bar(
+            x = census_subset['Name'],
+            y = census_subset['2010'])],
+    "layout": pgo.Layout(title = 'Selected State Populations in 2010 (in thousands)',
+                         xaxis = dict(
+                             title = 'State'),
+                         yaxis = dict(
+                             title = '2010 Population (thousands)'))
+    })
+
+        
